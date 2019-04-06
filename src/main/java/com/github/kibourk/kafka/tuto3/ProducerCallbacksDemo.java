fe299f94fc659a494c3e0a683c4a4a3bd370fdad
khadirIbourk <ibourk.khadir@gmail.com>
2019-04-06 14:54:40 +0100

    second commit

--- src/main/java/com/github/kibourk/kafka/tuto3/ProducerCallbacksDemo.java
+++ src/main/java/com/github/kibourk/kafka/tuto3/ProducerCallbacksDemo.java
@@ -1,15 +1,16 @@
 package com.github.kibourk.kafka.tuto3;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerCallbacks {
+public class ProducerCallbacksDemo {
 
     public static void main(String[] args){
+        final Logger logger= LoggerFactory.getLogger(ProducerCallbacksDemo.class);
         String bootstrapServers="127.0.0.1:9092";
         // create producer properties (Producer configs https://kafka.apache.org/documentation/#configuration)
         Properties properties=new Properties();
@@ -20,12 +21,28 @@ public class ProducerCallbacks {
         //create the producer
         KafkaProducer<String,String> producer=new KafkaProducer<String,String>(properties);
 
-        //create a producer record
-        ProducerRecord<String,String> record=new ProducerRecord<String, String>("first_topic","hello world from java producer");
+        for(int i=0;i<10;i++) {
+            //create a producer record
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world from java producer with callback "+Integer.toString(i));
 
-        //send data - asynchronous
-        producer.send(record);
-
+            //send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //to check if record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        //on success
+                        logger.info("received new metadata. \n" +
+                                "Topic:" + recordMetadata.topic() + "\n" +
+                                "Partition:" + recordMetadata.partition() + "\n" +
+                                "Offset:" + recordMetadata.offset() + "\n" +
+                                "Offset:" + recordMetadata.timestamp());
+                    } else {
+                        //on fail
+                        logger.error("Error while producing", e);
+                    }
+                }
+            });
+        }
         //flush the producer
         producer.flush();
 
