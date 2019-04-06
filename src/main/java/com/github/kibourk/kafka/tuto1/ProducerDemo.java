21cd0e9523c185b9d85c2be6de931fb0a0273ec7
khadirIbourk <ibourk.khadir@gmail.com>
2019-04-06 13:02:32 +0100

    initial commit

---
+++ src/main/java/com/github/kibourk/kafka/tuto1/ProducerDemo.java
@@ -0,0 +1,8 @@
+package com.github.kibourk.kafka.tuto1;
+
+public class ProducerDemo {
+    public static void main(String[] args){
+        //test
+        System.out.println("hello world!");
+    }
+}
